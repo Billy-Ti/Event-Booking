c7fc4ba18c0e7c00ed3f336359eb18325e15cb1b
Billy Ti <78161308+Billy-Ti@users.noreply.github.com>
2023-08-21 19:05:52 +0800

    Update : project update，see below 1. Modified BS JS path in main.js 2. Modified BS _variable.scss file 3. Add login verify 4. Add AOS effect in index.html

--- main.js
+++ main.js
@@ -1,3 +1,12 @@
 import './assets/scss/all.scss';
 
-console.log("Hello world!");
+// 引入 BS JS 檔
+import './node_modules/bootstrap/dist/js/bootstrap.bundle';
+
+// 引入 AOS 動畫套件
+import AOS from 'aos';
+import 'aos/dist/aos.css'; // You can also use <link> for styles
+// 初始化動畫套件
+AOS.init();
+
+
